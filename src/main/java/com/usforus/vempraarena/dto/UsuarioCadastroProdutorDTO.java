c79f9318c62851aa95d5122c587c73cd75ae4d7d
Elis Tenório <elismaidi@gmail.com>
2026-05-02 11:38:36 -0300

    feat: remove CPF field from UsuarioCadastroDTO and add UsuarioCadastroProdutorDTO for producer registration

---
+++ src/main/java/com/usforus/vempraarena/dto/UsuarioCadastroProdutorDTO.java
@@ -0,0 +1,42 @@
+package com.usforus.vempraarena.dto;
+
+public class UsuarioCadastroProdutorDTO {
+
+    private String cpf;
+    private String cnpj;
+    private String razaoSocial;
+    private String inscricaoMunicipal;
+
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getInscricaoMunicipal() {
+        return inscricaoMunicipal;
+    }
+
+    public void setInscricaoMunicipal(String inscricaoMunicipal) {
+        this.inscricaoMunicipal = inscricaoMunicipal;
+    }
+}
