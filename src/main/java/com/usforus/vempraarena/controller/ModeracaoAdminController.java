6108299aea0d02819453010afe46c2004cd92076
Elis Tenório <elismaidi@gmail.com>
2026-05-12 15:19:38 -0300

    feat: update admin area routing and enhance event approval functionality

--- src/main/java/com/usforus/vempraarena/controller/ModeracaoAdminController.java
+++ src/main/java/com/usforus/vempraarena/controller/ModeracaoAdminController.java
@@ -1,7 +1,10 @@
 package com.usforus.vempraarena.controller;
 
+import com.usforus.vempraarena.entities.Usuario;
+import com.usforus.vempraarena.repository.UsuarioRepository;
 import com.usforus.vempraarena.service.EventoService;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,23 +14,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/admin/eventos")
+@RequestMapping("/admin")
 @PreAuthorize("hasRole('ADMIN')")
 public class ModeracaoAdminController {
 
     private final EventoService eventoService;
+    private final UsuarioRepository usuarioRepository;
 
-    public ModeracaoAdminController(EventoService eventoService) {
+    public ModeracaoAdminController(EventoService eventoService, UsuarioRepository usuarioRepository) {
         this.eventoService = eventoService;
+        this.usuarioRepository = usuarioRepository;
     }
 
-    @GetMapping("/pendentes")
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Authentication authentication) {
+        Usuario usuario = usuarioRepository.findByEmail(authentication.getName());
+        model.addAttribute("usuario", usuario);
+        return "admin-area";
+    }
+
+
+    @GetMapping("/eventos/pendentes")
     public String painelAceite(Model model) {
         model.addAttribute("eventosPendentes", eventoService.listarEventosPendentes());
         return "aceite-eventos";
     }
 
-    @PostMapping("/aprovar/{id}")
+    @PostMapping("/eventos/aprovar/{id}")
     public String aprovar(@PathVariable Long id, RedirectAttributes redirectAttributes) {
         try {
             eventoService.aprovarEvento(id);
@@ -38,7 +52,7 @@ public class ModeracaoAdminController {
     }
 
     // 3. Botão de Rejeitar
-    @PostMapping("/rejeitar/{id}")
+    @PostMapping("/eventos/rejeitar/{id}")
     public String rejeitar(@PathVariable Long id, RedirectAttributes redirectAttributes) {
         try {
             eventoService.rejeitarEvento(id);
