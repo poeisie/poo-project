18de22418a4fb746cf6a786b7fc6ba90e0610e3e
Elis Tenório <elismaidi@gmail.com>
2026-05-15 18:53:33 -0300

    feat: implement DashboardService for statistical metrics calculation

--- src/main/java/com/usforus/vempraarena/repository/IngressoRepository.java
+++ src/main/java/com/usforus/vempraarena/repository/IngressoRepository.java
@@ -2,6 +2,8 @@ package com.usforus.vempraarena.repository;
 
 import com.usforus.vempraarena.entities.Ingresso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,8 @@ import java.util.List;
 public interface IngressoRepository  extends JpaRepository<Ingresso, Long> {
 
     List<Ingresso> findByUsuarioId(Long usuarioId);
+
+    @Query("SELECT i FROM Ingresso i WHERE i.evento.id IN :ids")
+    List<Ingresso> findByEventoIds(@Param("ids") List<Long> ids);
 
 }
